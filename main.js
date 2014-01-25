6e53c50b34149a9a91ae0be2968f5ecbfcb0f15b
blckshrk <alexand.bonhom@gmail.com>
2014-01-25 18:14:46 +0100

    Computing of the average speed and the total distance of each ride

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
 var loader = require('loader');
 var carbon = require('carbonfootprint');
+var fs = require('fs');
 
-var data = loader.loadData('data-xperium/2014-01-07.json');
+var data = loader.loadData('data-xperium/2014-01-17.json');
 
-var totalDistance = 0;
-for (var i = 1; i < data.length; i++) {
-	var lat1 = data[i-1].latitude;
-	var lon1 = data[i-1].longitude;
+// Iterate over all users data
+for (var u = 0; u < data.length; u++) {
+	console.log('User ' + u);
 	
-	var lat2 = data[i].latitude;
-	var lon2 = data[i].longitude;
-	
-	distance = carbon.computeDistance(lat1, lon1, lat2, lon2);
-	console.log(lat1 + "," + lon1 + " " + lat2 + "," + lon2 + " d: " +distance);
-	
-	totalDistance += distance;
+	// For each ride
+	data[u].forEach(function(ride, index) {
+		console.log('\nRide ' + index);
+		
+		var totalDistance = 0;
+		var averageSpeed = 0;
+		
+		var coordinates = ride.getCoordinates();
+		for (var i = 1; i < coordinates.length; i++) {
+			var t_1 = coordinates[i - 1];
+			var t = coordinates[i];
+			
+			var acc = t.accuracy.toFixed(1);
+			if (acc > 150) {
+				console.log('Bad accuracy: ' + acc);
+			}
+
+			// compute distance
+			totalDistance += t_1.distance(t);
+			
+			// aggregate speeds
+			var speed = t_1.speed(t);
+			averageSpeed += speed;
+			
+			var date = new Date(t.timestamp).toISOString().match(/(\d{2}:\d{2}:\d{2})/)[1];
+			console.log('[' + date + '] ' + t.toString() + ' (' + speed.toFixed(1) + ' km/h)');
+		}
+		
+		console.log('Total distance: ' + totalDistance.toFixed(1) + ' km');
+		console.log('Average speed: ' + (averageSpeed / (coordinates.length - 1)).toFixed(1) + ' km/h');
+	});
 }
-
-console.log('Total distance: ' + totalDistance);
