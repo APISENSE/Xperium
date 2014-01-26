2fa2fce22f2806b6e84f272b7f5ed444ad05e194
blckshrk <alexand.bonhom@gmail.com>
2014-01-26 19:28:59 +0100

    Adding support of multiple traces file accessible via url arguments (/map/:file/:user both avec integer starting from 0)

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 /**
  * Node.js server mail class
  */
-var express = require('express');
 
-// APISense data
+/*
+ * Processing data
+ */
 var loader = require('loader');
-var data = loader.loadData('data-xperium/2014-01-17.json');
+var data = loader.loadData('data-xperium');
 
+/*
+ * Running server
+ */
+var express = require('express');
 var app = express();
 console.log('Server started!');
 
-app.get('/map/:user', function(req, res) {
-	var userData = data[parseInt(req.params.user)];
+app.get('/map/:file/:user', function(req, res) {
+	var file = parseInt(req.params.file);
+	var user = parseInt(req.params.user);
+	
+	var userData = [];
+	if (file >= data.length) {
+		throw 'Attribute error: this file reference doesn\'t exists';
+	} else if(user >= data[file].length) {
+		throw 'Attribute error: this user reference doesn\'t exists';
+	} else {
+		userData = data[file][user];
+	}
 
     res.render('map.ejs', {data: userData});
 })
